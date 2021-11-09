ac81b64de36a47dd555787e42a0718cbcc6a8fae
minusInfinite <ashley@minusinfinite.net>
2021-11-09 11:40:23 +1030

    client setup

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,46 @@
 import { gql } from "apollo-server-core";
 
 const typeDefs = gql`
-    type Greeting {
-        _id: ID
-        message: String
+    type Service {
+        serviceNumber: String!
+        lastMessage: String
+        lastStatus: Boolean
+        messageCount: Int
+        usageCost: Float
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        serviceCount: Int
+        serviceList: [Service]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input ServiceInput {
+        serviceNumber: String!
+        lastMessage: String
+        lastStatus: Boolean
+        messageCount: Int
+        usageCost: Float
     }
 
     type Query {
-        hello: Greeting
+        me: User
     }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveService(service: ServiceInput!): User
+        removeService(serviceNumber: String): User
+    }
+
 `
 
 export default typeDefs
